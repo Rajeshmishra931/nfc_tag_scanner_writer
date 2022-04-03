bb5df2057f55b7273d8c70afab0338b5c4fb74c6
Rajesh Mishra <68376241+Rajeshmishra931@users.noreply.github.com>
2022-04-03 23:54:56 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// import nfc from './nfc.svg';
+import "./App.css";
+import Scan from "./containers/Scan";
+import Write from "./containers/Write";
+import { useState } from "react";
+import { ActionsContext } from "./contexts/context";
+
+function App() {
+  const [actions, setActions] = useState(null);
+  const { scan, write } = actions || {};
+
+  const actionsValue = { actions, setActions };
+
+  const onHandleAction = (actions) => {
+    setActions({ ...actions });
+  };
+
+  return (
+    <div className="App">
+      {/* <h1>NFC Tool</h1> */}
+      <div className="img"></div>
+      <div className="App-container">
+        <button
+          onClick={() => onHandleAction({ scan: "scanning", write: null })}
+          className="btn"
+        >
+          Scan
+        </button>
+        <button
+          onClick={() => onHandleAction({ scan: null, write: "writing" })}
+          className="btn"
+        >
+          Write
+        </button>
+      </div>
+      <ActionsContext.Provider value={actionsValue}>
+        {scan && <Scan />}
+        {write && <Write />}
+      </ActionsContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Write.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Writer from "../components/Writer/Writer";
+
+const Write = () => {
+  const onWrite = async (message) => {
+    try {
+      const ndef = new window.NDEFReader();
+      // This line will avoid showing the native NFC UI reader
+      await ndef.scan();
+      await ndef.write({ records: [{ recordType: "text", data: message }] });
+      alert(`Value Saved!`);
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
+  return <Writer writeFn={onWrite} />;
+};
+
+export default Write;
